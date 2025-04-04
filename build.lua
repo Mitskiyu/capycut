8b179e04d68ef44675f5957a81ba80e443f9a117
Mitskiyu <denzel.mitski@gmail.com>
2025-04-04 20:32:40 +0200

    feat: add build for 32 and 64bit

--- build.lua
+++ build.lua
@@ -1,23 +1,29 @@
 workspace "Capycut"
     configurations { "Debug", "Release" }
+    platforms { "Win32", "x64" } 
     location "build"
 
 project "Capycut"
     kind "ConsoleApp"
     language "C++"
     cppdialect "C++17"
-    targetdir "bin/%{cfg.buildcfg}"
+    targetdir "bin/%{cfg.buildcfg}-%{cfg.platform}"
 
     files { "src/**.cpp", "src/**.hpp" }
     links { "Ole32", "Shell32", "Uuid" }
 
-    filter "system:windows"
+    filter { "system:windows", "platforms:Win32" }
         systemversion "latest"
-        
+        architecture "x86"
+
+    filter { "system:windows", "platforms:x64" }
+        systemversion "latest"
+        architecture "x86_64"
+
     filter "configurations:Debug"
         defines { "DEBUG" }
         symbols "On"
-        
+
     filter "configurations:Release"
         defines { "NDEBUG" }
         optimize "On"
