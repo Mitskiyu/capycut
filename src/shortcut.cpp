38a009d2a5f0ce57d3c45d2a7defc487acc6d6f2
Mitskiyu <denzel.mitski@gmail.com>
2025-04-04 17:45:40 +0200

    feat: add create shortcut

--- src/shortcut.cpp
+++ src/shortcut.cpp
@@ -1,7 +1,10 @@
 #include <codecvt>
 #include <filesystem>
+#include <fstream>
+#include <iosfwd>
 #include <iostream>
 #include <locale>
+#include <regex>
 #include <string>
 
 #include <combaseapi.h>
@@ -10,6 +13,7 @@
 #include <windows.h>
 
 #include "shortcut.hpp"
+#include "steam.hpp"
 
 std::wstring Shortcut::LocateStartMenuPath()
 {
@@ -34,4 +38,43 @@ std::wstring Shortcut::LocateStartMenuPath()
 	std::wstring startMenuPath(path);
 	CoTaskMemFree(path);
 	return startMenuPath;
+}
+
+std::wstring Shortcut::CreateSubDir(const std::wstring& startMenuPath)
+{
+	std::wstring subDir = L"Capycut";
+	std::wstring fullPath = startMenuPath + L"\\" + subDir;
+
+	std::filesystem::create_directories(fullPath);
+
+	return fullPath;
+}
+
+void Shortcut::CreateShortcut(const Steam::AppData& data, const std::wstring& subDirPath)
+{
+	// invalid filename characters
+	std::regex invalid(R"([\\/*?:"<>;|])");
+
+	// sanitize the filename, replace with _
+	std::string name = std::regex_replace(data.name, invalid, "_");
+
+	// convert to wstring for filename
+	std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> conv;
+	std::wstring wname = conv.from_bytes(name);
+
+	std::wstring wfilePath = subDirPath + L"\\" + wname + L".url";
+
+	std::string filePath = conv.to_bytes(wfilePath);
+	std::ofstream ofs(filePath);
+	if (!ofs)
+	{
+		std::wcerr << L"ERROR: Could not create shortcut for file: " << wfilePath << '\n';
+		return;
+	}
+
+	// write url file
+	ofs << "[InternetShortcut]\n";
+	ofs << "URL=steam://rungameid/" << data.appId << '\n';
+
+	ofs.close();
 }
--- src/shortcut.hpp
+++ src/shortcut.hpp
@@ -3,16 +3,21 @@
 
 #include <string>
 
+#include "steam.hpp"
+
 class Shortcut
 {
 public:
-	Shortcut() : startMenuPath(LocateStartMenuPath())
+	Shortcut() : startMenuPath(LocateStartMenuPath()), subDirPath(startMenuPath)
 	{
 	}
 
 	std::wstring getStartMenuPath() const { return startMenuPath; }
+	static std::wstring CreateSubDir(const std::wstring& startMenuPath);
+	static void CreateShortcut(const Steam::AppData& data, const std::wstring& output);
 private:
 	std::wstring startMenuPath;
+	std::wstring subDirPath;
 
 	static std::wstring LocateStartMenuPath();
 };
